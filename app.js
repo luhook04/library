408208d6e0109160286923bec97975b1fdd5090d
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-10-05 23:03:53 -0400

    set up some variables and the constructor function

---
+++ app.js
@@ -0,0 +1,13 @@
+// reference variables
+const addBtn = document.getElementById('addBook');
+const submitBtn = document.getElementById('submit-button');
+
+// initialize array to store information
+function Book(title, author, pages, read) {
+  this.title = form.title.value;
+  this.author = form.author.value;
+  this.pages = form.author.value + 'pages';
+  this.read = form.read.checked;
+}
+
+const dog = new Book(title, author, pages, read);
